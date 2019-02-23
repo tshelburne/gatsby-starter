039dd4e232a9cc7e0253eee0a58c23aa41afa2a4
Tim Shelburne <shelburt02@gmail.com>
2019-02-22 23:57:04 -0500

    formatting

--- src/util/react.js
+++ src/util/react.js
@@ -1,21 +1,21 @@
 import React from 'react'
 
 function elementHasType(Component) {
-  return element => element.type === React.createElement(Component).type
+	return element => element.type === React.createElement(Component).type
 }
 
 export function oneByType(children, Component) {
-  return React.Children.toArray(children).find(elementHasType(Component))
+	return React.Children.toArray(children).find(elementHasType(Component))
 }
 
 export function allByType(children, Component) {
-  return React.Children.toArray(children).filter(elementHasType(Component))
+	return React.Children.toArray(children).filter(elementHasType(Component))
 }
 
 export function withoutTypes(children, ...Components) {
-  const types = Components.map(C => React.createElement(C).type)
+	const types = Components.map(C => React.createElement(C).type)
 
-  return React.Children.toArray(children).filter(
-    child => !types.includes(child.type)
-  )
+	return React.Children.toArray(children).filter(
+		child => !types.includes(child.type)
+	)
 }
